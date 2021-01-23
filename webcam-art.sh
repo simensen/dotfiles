8e06200006f52f5ee14786b2be7b95f73b2dbfc6
Beau Simensen <beau@dflydev.com>
2021-01-22 18:25:24 -0600

    Webcam art script

---
+++ webcam-art.sh
@@ -0,0 +1,12 @@
+uvcc set auto_focus 0
+uvcc set absolute_focus 1
+#uvcc set absolute_zoom 140
+uvcc set absolute_zoom 120
+uvcc set auto_white_balance_temperature 0
+uvcc set white_balance_temperature 5800
+uvcc set auto_exposure_mode 1
+uvcc set absolute_exposure_time 200
+uvcc set gain 50
+#uvcc set absolute_pan_tilt 7200 36000
+uvcc set absolute_pan_tilt 0 36000
+#uvcc set absolute_pan_tilt 0 0
