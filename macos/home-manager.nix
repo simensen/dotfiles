811412a421fa6907b6b45f8abfc1e8a902d30e73
Beau Simensen <beau@dflydev.com>
2023-02-23 15:47:11 -0600

    Move mas to its own file

--- macos/home-manager.nix
+++ macos/home-manager.nix
@@ -3,6 +3,7 @@
 let
   common-programs = import ../common/home-manager.nix { config = config; pkgs = pkgs; lib = lib; };
   home-manager-real = import ../common/home-manager-real.nix { config = config; pkgs = pkgs; lib = lib; };
+  mas-apps = import ./mas-apps.nix { };
 in
 {
   imports = [
@@ -66,25 +67,26 @@ in
   #  "mutagen-io/mutagen/mutagen"
   #];
   homebrew.casks = pkgs.callPackage ./casks.nix {};
-  homebrew.masApps = {
-    "Bear" = 1091189122;
-    "Controller" = 1198176727;
-    "Focus - Time Management" = 777233759;
-    "GoodNotes 5" = 1444383602;
-    "HomeControl" = 1547121417;
-    "Microsoft Excel" = 462058435;
-    "Microsoft PowerPoint" = 462062816;
-    "Microsoft Word" = 462054704;
-    "MindNode" = 1289197285;
-    "Petrify" = 1451177988;
-    "Reeder" = 1529448980;
-    "Spark – Email App by Readdle" = 1176895641;
-    "Speedtest" = 1153157709;
-    "Todoist" = 585829637;
-    "ToothFairy" = 1191449274;
-    "Trello" = 1278508951;
-    "Vectornator" = 1219074514;
-  };
+  homebrew.masApps = mas-apps;
+  #homebrew.masApps = {
+  #  "Bear" = 1091189122;
+  #  "Controller" = 1198176727;
+  #  "Focus - Time Management" = 777233759;
+  #  "GoodNotes 5" = 1444383602;
+  #  "HomeControl" = 1547121417;
+  #  "Microsoft Excel" = 462058435;
+  #  "Microsoft PowerPoint" = 462062816;
+  #  "Microsoft Word" = 462054704;
+  #  "MindNode" = 1289197285;
+  #  "Petrify" = 1451177988;
+  #  "Reeder" = 1529448980;
+  #  "Spark – Email App by Readdle" = 1176895641;
+  #  "Speedtest" = 1153157709;
+  #  "Todoist" = 585829637;
+  #  "ToothFairy" = 1191449274;
+  #  "Trello" = 1278508951;
+  #  "Vectornator" = 1219074514;
+  #};
 
 
   home-manager = {
---
+++ macos/mas-apps.nix
@@ -0,0 +1,21 @@
+{ ... }:
+
+{
+  "Bear" = 1091189122;
+  "Controller" = 1198176727;
+  "Focus - Time Management" = 777233759;
+  "GoodNotes 5" = 1444383602;
+  "HomeControl" = 1547121417;
+  "Microsoft Excel" = 462058435;
+  "Microsoft PowerPoint" = 462062816;
+  "Microsoft Word" = 462054704;
+  "MindNode" = 1289197285;
+  "Petrify" = 1451177988;
+  "Reeder" = 1529448980;
+  "Spark – Email App by Readdle" = 1176895641;
+  "Speedtest" = 1153157709;
+  "Todoist" = 585829637;
+  "ToothFairy" = 1191449274;
+  "Trello" = 1278508951;
+  "Vectornator" = 1219074514;
+}
