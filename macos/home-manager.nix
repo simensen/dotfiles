b41e88937fe571b1f809cf125080972652c4aed9
Beau Simensen <beau@dflydev.com>
2023-06-13 09:41:55 -0500

    Minor updates, new apps

--- macos/home-manager.nix
+++ macos/home-manager.nix
@@ -4,6 +4,7 @@ let
   common-programs = import ../common/home-manager.nix { config = config; pkgs = pkgs; lib = lib; };
   home-manager-real = import ../common/home-manager-real.nix { config = config; pkgs = pkgs; lib = lib; };
   mas-apps = import ./mas-apps.nix { };
+  brews = import ./brews.nix { };
 in
 {
   imports = [
@@ -68,26 +69,7 @@ in
   #];
   homebrew.casks = pkgs.callPackage ./casks.nix {};
   homebrew.masApps = mas-apps;
-  #homebrew.masApps = {
-  #  "Bear" = 1091189122;
-  #  "Controller" = 1198176727;
-  #  "Focus - Time Management" = 777233759;
-  #  "GoodNotes 5" = 1444383602;
-  #  "HomeControl" = 1547121417;
-  #  "Microsoft Excel" = 462058435;
-  #  "Microsoft PowerPoint" = 462062816;
-  #  "Microsoft Word" = 462054704;
-  #  "MindNode" = 1289197285;
-  #  "Petrify" = 1451177988;
-  #  "Reeder" = 1529448980;
-  #  "Spark – Email App by Readdle" = 1176895641;
-  #  "Speedtest" = 1153157709;
-  #  "Todoist" = 585829637;
-  #  "ToothFairy" = 1191449274;
-  #  "Trello" = 1278508951;
-  #  "Vectornator" = 1219074514;
-  #};
-
+  homebrew.brews = brews;
 
   home-manager = {
     useGlobalPkgs = true;
@@ -106,7 +88,6 @@ in
       editorconfig.enable = true;
     };
   };
-
 
   # Add home-manager applications to `system.build.applications` so they will be linked
   # by services.link-apps.
